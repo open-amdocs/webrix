d55c997e24cca9ae0cc6774c384cb7c00de264d0
yoavka <yoav.kadosh@amdocs.com>
2020-05-01 10:32:19 +0300

    Added source file notices

--- notice.js
+++ notice.js
@@ -4,12 +4,3 @@
  * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-import React from 'react';
-import './HelloWorld.scss';
-
-const HelloWorld = () => (
-    <div className='hello-world'>Hello World!</div>
-);
-
-export default HelloWorld;
