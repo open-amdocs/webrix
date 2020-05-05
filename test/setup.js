f58aeff74dea6c26a502713009dfa14b0c7d383f
UDIA <udi.avganim@amdocs.com>
2020-05-05 16:06:16 +0300

    Fixing test and lint

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,15 @@
 import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {JSDOM} from 'jsdom';
+import sinon from 'sinon';
 
 configure({adapter: new Adapter()});
+
+//JSDOM configuration
+const dom = new JSDOM(
+    '<!DOCTYPE html><html><body></body></html>',
+    {url: 'http://localhost/'}
+);
+global.window = dom.window;
+global.document = dom.window.document;
+global.document.addEventListener = sinon.spy();
