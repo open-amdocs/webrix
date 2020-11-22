aef92f9f3cfc0bba4ce6ba6bd1714f2d1d106827
yoavka <yoavks@gmail.com>
2020-11-22 14:05:36 +0200

    Added object utils

--- src/utility/object/object.test.js
+++ src/utility/object/object.test.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {get} from './object';
+import {get, set, isEqual, omit, clone} from './object';
 
 describe('Object', () => {
 
@@ -46,7 +46,73 @@ describe('Object', () => {
         expect(get({name: false}, 'name', 'default')).to.eql(false);
         expect(get({name: null}, 'name', 'default')).to.eql(null);
         expect(get({name: 'Jack'}, 'name', 'default')).to.eql('Jack');
-
     });
 
+    it('set()', () => {
+        expect(set({}, 'foo', 'bar')).to.eql({foo: 'bar'});
+        expect(set({foo: 'foo'}, 'foo', 'bar')).to.eql({foo: 'bar'});
+        expect(set({}, 'foo', 'bar')).to.eql({foo: 'bar'});
+        expect(set({}, 'foo.bar', 'bar')).to.eql({foo: {bar: 'bar'}});
+        expect(set({}, 'foo.bar[0]', 'bar')).to.eql({foo: {bar: ['bar']}});
+        expect(set({}, 'foo.bar[0].foo', 'bar')).to.eql({foo: {bar: [{foo: 'bar'}]}});
+        expect(set({}, 'foo.bar[0][0]', 'bar')).to.eql({foo: {bar: [['bar']]}});
+        expect(set({}, 'foo.bar[1]', 'bar')).to.eql({foo: {bar: [undefined, 'bar']}});
+        expect(set([], '[0]', 'bar')).to.eql(['bar']);
+        expect(set([], '[0][0]', 'bar')).to.eql([['bar']]);
+    });
+
+    it('isEqual()', () => {
+        expect(isEqual(1, 1)).to.eql(true);
+        expect(isEqual(true, false)).to.eql(false);
+        expect(isEqual('hello', 'hello')).to.eql(true);
+        expect(isEqual([1, 2, 3], [1, 2, 3])).to.eql(true);
+        expect(isEqual([1, 2, 3], [3, 2, 1])).to.eql(false);
+        expect(isEqual([{id: 1}], [{id: 2}])).to.eql(false);
+        expect(isEqual({foo: 'bar'}, {foo: 'bar'})).to.eql(true);
+        expect(isEqual({foo: 'bar'}, {bar: 'foo'})).to.eql(false);
+        expect(isEqual({foo: [1, 2, {a: 1}]}, {foo: [1, 2, {a: 1}]})).to.eql(true);
+        expect(isEqual({foo: [1, 2, {a: 1}]}, {foo: [1, 2, {a: 2}]})).to.eql(false);
+
+        /* Check Circular Traversal */
+        const sub1 = {foo: [1, 2, {a: 1}]};
+        const sub2 = {foo: [1, 2, {a: 1}]};
+        const parent1 = {foo: [1, 2, {a: 1}], sub1, sub2};
+        const parent2 = {foo: [1, 2, {a: 1}], sub1, sub2};
+        parent1.aaa = parent2;
+        parent1.bbb = parent1;
+        parent2.aaa = parent1;
+        parent2.bbb = parent2;
+        expect(isEqual(parent1, parent2)).to.eql(true);
+        parent2.ccc = 6;
+        expect(isEqual(parent1, parent2)).to.eql(false);
+    });
+
+    it('clone()', () => {
+        const source = {foo: 1, bar: 'hello', baz: {baq: 23, test: [1, 2, 3]}};
+        const target = clone(source);
+
+        expect(source).to.eql(target);
+        expect(source === target).to.eql(false);
+        expect(source.baz === target.baz).to.eql(false);
+        expect(source.baz.test === target.baz.test).to.eql(false);
+
+        expect([1, 2, 3]).to.eql(clone([1, 2, 3]));
+        expect([{foo: 'bar'}, {}, 'foo', []]).to.eql(clone([{foo: 'bar'}, {}, 'foo', []]));
+        expect([{foo: {bar: 'foobar'}}, {foo: [1, 2, 3]}]).to.eql(clone([{foo: {bar: 'foobar'}}, {foo: [1, 2, 3]}]));
+        expect({foo: {bar: 'foobar'}, bar: []}).to.eql(clone({foo: {bar: 'foobar'}, bar: []}));
+    });
+
+    it('omit()', () => {
+        const obj = {a: 1, b: 2, c: {d: {e: 3}}, f: [{g: 4}, {h: 5}]};
+
+        expect(omit()).to.eql(undefined);
+        expect(omit({})).to.eql({});
+        expect(omit(obj)).to.eql(obj);
+        expect(omit(obj, 'a')).to.not.have.property('a');
+        expect(omit(obj, 'a', 'b')).to.eql({c: {d: {e: 3}}, f: [{g: 4}, {h: 5}]});
+        expect(omit(obj, 'a', 'b', 'c.d')).to.eql({c: {}, f: [{g: 4}, {h: 5}]});
+        expect(omit(obj, 'a', 'b', 'c.d.e')).to.eql({c: {d: {}}, f: [{g: 4}, {h: 5}]});
+        expect(omit(obj, 'f[0]')).to.eql({a: 1, b: 2, c: {d: {e: 3}}, f: [undefined, {h: 5}]});
+        expect(omit(obj, 'f[0].g')).to.eql({a: 1, b: 2, c: {d: {e: 3}}, f: [{}, {h: 5}]});
+    });
 });
