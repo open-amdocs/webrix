93c6dce67173541fc2e3665d5c24bb08dd743e9e
yoavka <yoavks@gmail.com>
2021-01-20 16:10:10 +0200

    feat(): Exposing Scrollable utils & default scrollbars

--- src/components/Scrollable/index.js
+++ src/components/Scrollable/index.js
@@ -1,8 +1,13 @@
 import Scrollable from './Scrollable';
-import {VerticalScrollbarPlaceholder, HorizontalScrollbarPlaceholder, Shadow}  from './components';
+import {VerticalScrollbar, HorizontalScrollbar, VerticalScrollbarPlaceholder, HorizontalScrollbarPlaceholder, Shadow}  from './components';
+import {getThumbLength, getThumbPosition} from './Scrollable.utils';
 
 Scrollable.VerticalScrollbar = VerticalScrollbarPlaceholder;
+Scrollable.VerticalScrollbar.Default = VerticalScrollbar;
 Scrollable.HorizontalScrollbar = HorizontalScrollbarPlaceholder;
+Scrollable.HorizontalScrollbar.Default = HorizontalScrollbar;
+Scrollable.getThumbLength = getThumbLength;
+Scrollable.getThumbPosition = getThumbPosition;
 Scrollable.Shadow = Shadow;
 
 export default Scrollable;
