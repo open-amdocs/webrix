dbcaec8ba969ccaaa283b43852f7f5cb56ae1796
yoavka <yoavks@gmail.com>
2021-02-11 01:20:12 +0200

    Removed unused util

--- src/components/Movable/Movable.utils.js
+++ src/components/Movable/Movable.utils.js
@@ -14,20 +14,6 @@
  * limitations under the License.
  */
 
-import {clamp, toCSS} from 'utility/rect';
-
-/**
- *
- * @param rect {{top: number, left: number, width: number, height: number}}
- * @param container {{top: number, left: number, width: number, height: number}}
- * @return {{top: number, left: number, width: number, height: number}}
- */
-export const inscribe = (rect, container) => toCSS(clamp(
-    rect,
-    {...rect, left: container.left + container.width - rect.width, top: container.top + container.height - rect.height},
-    {...rect, left: container.left, top: container.top}
-));
-
 /**
  * Call the given handlers with a normalized version of a move event.
  *
