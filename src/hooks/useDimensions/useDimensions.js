46de33b8824f73b49f91a7e0d76f161cb996e869
yoavka <yoavks@gmail.com>
2021-02-18 15:39:07 +0200

    feat(): Added useDimensions()

---
+++ src/hooks/useDimensions/useDimensions.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2020, Amdocs Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {useState, useRef, useEffect} from 'react';
+
+export default ref => {
+    const [dimensions, setDimensions] = useState({width: 0, height: 0});
+    const observer = useRef(new window.ResizeObserver(entries => {
+        const {inlineSize: width, blockSize: height} = entries[0].borderBoxSize[0];
+        setDimensions({width, height});
+    }));
+    useEffect(() => {
+        observer.current.observe(ref.current);
+        return () => observer.current.disconnect();
+    }, []);
+    return dimensions;
+};
