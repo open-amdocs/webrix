1e6e58ce74a8797e20b03f3600ffdbdeb26402e2
benta <tamirben@gmail.com>
2021-06-01 17:03:01 +0300

    remove unused var

--- src/components/Scrollable/Scrollable.test.js
+++ src/components/Scrollable/Scrollable.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {mount, shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import {expect} from 'chai';
 import sinon from 'sinon';
 import Scrollable from './Scrollable';
