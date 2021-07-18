898f9874a5bcd8d162cd03b364c0261acb321ba3
yoavka <yoavks@gmail.com>
2021-07-18 15:53:21 +0300

    Added NYC configuration to jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,19 @@
-  module.exports = async () => {
+module.exports = async () => {
     return {
         verbose: true,
+        coverageReporters: ['lcov', 'text-summary', 'html'],
+        collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/index.js', '!src/utility/mocks/mocks.js'],
+        coverageThreshold: {
+            global: {
+                'branches': 90,
+                'functions': 90,
+                'lines': 90,
+                'statements': 90,
+            },
+        },
         setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
         moduleNameMapper: {
             '\.(css)$': '<rootDir>/test/mocks/styleMock.js',
-        }
+        },
     };
 };
