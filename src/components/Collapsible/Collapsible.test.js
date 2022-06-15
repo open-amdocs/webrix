3d596befcd08cc68862ca0d5b574f2be46cb0ad9
Yair Even Or <Yair.Even-Or@amdocs.com>
2022-06-15 17:38:31 +0300

    - Removed `content-wrapper` div element, as it wasn't needed - Added namespace constant for dynamic selectors names - Refactored selectors and changed to BEM naming, for improved isolation - Tests now using the (exported) namespace - TBD: use dynamic prefix imported from a centralized place

--- src/components/Collapsible/Collapsible.test.js
+++ src/components/Collapsible/Collapsible.test.js
@@ -2,24 +2,24 @@ import React from 'react';
 import {mount} from 'enzyme';
 import sinon from 'sinon';
 import {act} from 'react-dom/test-utils';
-import Collapsible from './Collapsible';
+import Collapsible, {NAMESPACE} from './Collapsible';
 
 describe('<Collapsible/>', () => {
     describe('HTML Structure', () => {
         it('should render empty Collapsible if no children supplied', () => {
             const wrapper = mount(<Collapsible></Collapsible>);
-            expect(wrapper.find('.collapsible').children()).toHaveLength(0)
+            expect(wrapper.find(`.${NAMESPACE}`).children()).toHaveLength(0)
             wrapper.unmount();
         });
 
         it('should render a collapsed Collapsible', () => {
             const wrapper = mount(<Collapsible>foo</Collapsible>);
-            expect(wrapper.find('.collapsible')).toHaveLength(1);
+            expect(wrapper.find(`.${NAMESPACE}`)).toHaveLength(1);
             wrapper.unmount();
         });
         it('should render an expanded Collapsible', () => {
             const wrapper = mount(<Collapsible expanded>foo</Collapsible>);
-            expect(wrapper.find('.collapsible.expanded')).toHaveLength(1);
+            expect(wrapper.find(`.${NAMESPACE}--expanded`)).toHaveLength(1);
             wrapper.unmount();
         });
     });
@@ -27,7 +27,7 @@ describe('<Collapsible/>', () => {
     describe('Props', () => {
         it('should support custom class name', () => {
             const wrapper = mount(<Collapsible className='bar'>foo</Collapsible>);
-            expect(wrapper.find('.collapsible.bar')).toHaveLength(1);
+            expect(wrapper.find(`.${NAMESPACE}.bar`)).toHaveLength(1);
             wrapper.unmount();
         });
     });
@@ -38,40 +38,40 @@ describe('<Collapsible/>', () => {
                 const onTransitionEnd = sinon.spy();
                 const wrapper = mount(<Collapsible onTransitionEnd={onTransitionEnd}><div/></Collapsible>);
 
-                wrapper.find('.content-wrapper').prop('onTransitionEnd')({propertyName: 'transform'});
+                wrapper.find(`.${NAMESPACE}`).prop('onTransitionEnd')({propertyName: 'transform'});
                 expect(onTransitionEnd.callCount).toEqual(1);
 
-                wrapper.find('.content-wrapper').prop('onTransitionEnd')({propertyName: 'width'});
+                wrapper.find(`.${NAMESPACE}`).prop('onTransitionEnd')({propertyName: 'width'});
                 expect(onTransitionEnd.callCount).toEqual(1);
                 wrapper.unmount();
             });
         });
         it('should toggle from collapsed to expanded', async () => {
             const wrapper = mount(<Collapsible>foo</Collapsible>);
-            expect(wrapper.find('.collapsible.expanded')).toHaveLength(0);
+            expect(wrapper.find(`.${NAMESPACE}--expanded`)).toHaveLength(0);
 
             wrapper.setProps({expanded: true});
             wrapper.update();
-            expect(wrapper.find('.collapsible.expanded')).toHaveLength(0); // should not have the "expanded" until transition is done
-            expect(wrapper.find('.collapsible.expanding')).toHaveLength(1);
+            expect(wrapper.find(`.${NAMESPACE}--expanded`)).toHaveLength(0); // should not have the "expanded" until transition is done
+            expect(wrapper.find(`.${NAMESPACE}--expanding`)).toHaveLength(1);
 
-            wrapper.find('.content-wrapper').simulate('transitionEnd', {propertyName: 'transform'});
+            wrapper.find(`.${NAMESPACE}`).simulate('transitionEnd', {propertyName: 'transform'});
 
-            expect(wrapper.find('.collapsible.expanding')).toHaveLength(0); // expading class should be removed once transition is done
+            expect(wrapper.find(`.${NAMESPACE}--expanding`)).toHaveLength(0); // expading class should be removed once transition is done
             wrapper.unmount();
         });
         it('should toggle from expanded to collapsed', async () => {
             const wrapper = mount(<Collapsible expanded>foo</Collapsible>);
-            expect(wrapper.find('.collapsible.expanded')).toHaveLength(1);
+            expect(wrapper.find(`.${NAMESPACE}--expanded`)).toHaveLength(1);
 
             wrapper.setProps({expanded: false});
             wrapper.update();
-            expect(wrapper.find('.collapsible.collapsing')).toHaveLength(1);
+            expect(wrapper.find(`.${NAMESPACE}--collapsing`)).toHaveLength(1);
 
-            wrapper.find('.content-wrapper').simulate('transitionEnd', {propertyName: 'transform'});
+            wrapper.find(`.${NAMESPACE}`).simulate('transitionEnd', {propertyName: 'transform'});
 
-            expect(wrapper.find('.collapsible.collapsing')).toHaveLength(0);
-            expect(wrapper.find('.collapsible').exists()).toBe.true;
+            expect(wrapper.find(`.${NAMESPACE}--collapsing`)).toHaveLength(0);
+            expect(wrapper.find(`.${NAMESPACE}`).exists()).toBe.true;
             wrapper.unmount();
         });
     });
