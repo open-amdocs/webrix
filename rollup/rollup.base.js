50f42c859789cb472893a3abb5a82117056f80c9
Yair Even Or <Yair.Even-Or@amdocs.com>
2022-06-26 23:02:39 +0300

    using `rollup/plugin-replace` for global prefix injections `wx-` for classnames

--- rollup/rollup.base.js
+++ rollup/rollup.base.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 
 export default {
     input: 'src/index.js',
@@ -12,6 +13,7 @@ export default {
         'classnames',
     ],
     plugins: () => ([// Must be a function so that plugin's instances are regenerated for every config
+        replace({'{{PREFIX}}': 'wx-', preventAssignment: true, delimiters: ['', '']}),
         resolve({extensions: ['.js', '.jsx']}),
         commonjs({include: /node_modules/}),
         babel({babelHelpers: 'runtime', exclude: /node_modules/}),
