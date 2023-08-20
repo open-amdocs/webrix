48a4a4b2b690c6d4ebf4b543015e0c661a900574
Yair Even Or <vsync.design@gmail.com>
2023-08-20 15:46:40 +0300

    scrollable tracks should not inherit css from ancestors (#119)
    Co-authored-by: Yair Even Or <Yair.Even-Or@amdocs.com>

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
-module.exports = async () => {
-    return {
-        testEnvironment: 'jsdom',
-        verbose: true,
-        coverageReporters: ['lcov', 'text-summary', 'html'],
-        collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/index.js', '!**/*.props.js', '!**/*.context.js', '!src/utility/mocks/mocks.js'],
-        coverageThreshold: {
-            global: {
-                'branches': 90,
-                'functions': 90,
-                'lines': 90,
-                'statements': 90,
-            },
+module.exports = async () => ({
+    testEnvironment: 'jsdom',
+    verbose: true,
+    coverageReporters: ['lcov', 'text-summary', 'html'],
+    collectCoverageFrom: ['src/**/*.{js,jsx}', '!**/index.js', '!**/*.props.js', '!**/*.context.js', '!src/utility/mocks/mocks.js'],
+    coverageThreshold: {
+        global: {
+            'branches': 90,
+            'functions': 90,
+            'lines': 90,
+            'statements': 90,
         },
-        setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
-        moduleNameMapper: {
-            '.(css)$': '<rootDir>/test/mocks/styleMock.js',
-        },
-    };
-};
+    },
+    setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+    moduleNameMapper: {
+        '.(css)$': '<rootDir>/test/mocks/styleMock.js',
+    },
+});
